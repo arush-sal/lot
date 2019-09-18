204c3213c4acb8810ae246b27dada0793bd15c1f
Arush Salil <me@aru.sh>
2019-09-18 15:06:39 +0530

    THIS IS SPARTAAAAAAAAAAAAAAAAAAAAA
    - Add process tree feature
    
    - Update the go modules
    
    - Fix linting issues in procutil
    
    - Add procutil.PrintProcessTree() to the tree command
    
    Signed-off-by: Arush Salil <me@aru.sh>

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/arush-sal/lot
 go 1.12
 
 require (
+	github.com/disiqueira/gotree v1.0.0
 	github.com/spf13/cobra v0.0.5
 	golang.org/x/sys v0.0.0-20181205085412-a5c9d58dba9a
 )
---
+++ pkg/util/procutil/tree.go
@@ -0,0 +1,72 @@
+/*
+	Copyright © 2019 LoT Authors
+
+	Licensed under the Apache License, Version 2.0 (the "License");,
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+package procutil
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+
+	"github.com/arush-sal/lot/pkg/util"
+	"github.com/disiqueira/gotree"
+)
+
+type process struct {
+	Name string
+	Pid  string
+}
+
+// getProcessTree returns a tree with process ancestral hierarchy
+func getProcessTree() gotree.Tree {
+	list := make(map[int][]*process)
+	processes, err := GetProcessStats()
+	util.ErrorCheck(err)
+
+	for _, ps := range processes {
+		p := new(process)
+		p.Name = ps.Name
+		p.Pid = ps.Pid
+		list[ps.Ppid] = append(list[ps.Ppid], p)
+	}
+
+	ppids := []int{}
+
+	for key := range list {
+		ppids = append(ppids, key)
+	}
+
+	sort.Ints(ppids)
+	var gparent = gotree.New("0")
+	for _, ppid := range ppids {
+		if ppid == 0 {
+			continue
+		}
+		parent := gotree.New(strconv.FormatInt(int64(ppid), 10))
+
+		for _, ps := range list[ppid] {
+			processInfo := ps.Name + "(" + ps.Pid + ")"
+			parent.Add(processInfo)
+		}
+		gparent.AddTree(parent)
+	}
+	return gparent
+}
+
+// PrintProcessTree prints a tree of processes with their ancestral hierarchy
+func PrintProcessTree() {
+	fmt.Println(getProcessTree().Print())
+}
